a1c3de20c49a3987b0f1ec126d210ccdfa94b9b2
mcferrenm <mcferrenmax@gmail.com>
2019-05-08 17:12:32 -0400

    Max heap completed

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -7,18 +7,24 @@ class Heap:
         self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        pass
-        if len(self.storage) <= 1:
-            self.storage.pop()
+
+        if not len(self.storage):
+            return None
+        elif len(self.storage) == 1:
+            return self.storage.pop()
         else:
-            # current_max = self.storage[0] why do we store the number we are deleting?
+            current_max = self.storage[0]
             self.storage[0] = self.storage.pop()
 
+            self._sift_down(0)
+
+            return current_max
+
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
         # loop until either element reaches the top of the array
@@ -41,16 +47,43 @@ class Heap:
             index = parent
 
     def _sift_down(self, index):
-        pass
+        while index < len(self.storage) - 1:
+
+            left_child = 2 * index + 1
+            right_child = 2 * index + 2
+
+            if left_child <= len(self.storage) - 1 and right_child <= len(self.storage) - 1:
+                if self.storage[left_child] > self.storage[right_child]:
+                    if self.storage[index] < self.storage[left_child]:
+                        self.storage[index], self.storage[left_child] = self.storage[left_child], self.storage[index]
+                else:
+                    if self.storage[index] < self.storage[right_child]:
+                        self.storage[index], self.storage[right_child] = self.storage[right_child], self.storage[index]
+            elif left_child <= len(self.storage) - 1:
+                if self.storage[index] < self.storage[left_child]:
+                    self.storage[index], self.storage[left_child] = self.storage[left_child], self.storage[index]
+            index += 1
 
 
 h = Heap()
 
-# print(h.storage)
-h.insert(10)
+h.insert(6)
 h.insert(7)
-# # h.delete()
-print(h.storage)
+h.insert(5)
+h.insert(8)
+h.insert(10)
+h.insert(1)
+h.insert(2)
+h.insert(5)
+
+
+# print(h.get_size())
+# print(h.delete())
+
+descending_order = []
+
+while h.get_size() > 0:
+    descending_order.append(h.delete())
 
 """
 l = 2i + 1
