9754dbd348dccca51b94b23721738087508975ee
mcferrenm <mcferrenmax@gmail.com>
2019-05-08 17:14:16 -0400

    Clean up

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -7,7 +7,6 @@ class Heap:
         self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-
         if not len(self.storage):
             return None
         elif len(self.storage) == 1:
@@ -27,28 +26,18 @@ class Heap:
         return len(self.storage)
 
     def _bubble_up(self, index):
-        # loop until either element reaches the top of the array
-        # or we'll break the loop when we realize the element's priority
-        # is not larger than its parent's value
         while index > 0:
-            # the value at 'index' fetches the index of its parent
-            parent = (index - 1) // 2  # includes floor
-            # check if the element at 'index' has higher priority than
-            # the elment at parent index
-            if self.storage[index] > self.storage[parent]:
-                # then we need to swap the elements
-                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+            parent = (index - 1) // 2
 
-            # update the index to move up the tree (parent index)
+            if self.storage[index] > self.storage[parent]:
+                self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
             else:
-                # otherwise, our element has reached a spot in the heap where
-                # its parent element has higher priority; stop climbing
                 break
+
             index = parent
 
     def _sift_down(self, index):
         while index < len(self.storage) - 1:
-
             left_child = 2 * index + 1
             right_child = 2 * index + 2
 
@@ -63,30 +52,3 @@ class Heap:
                 if self.storage[index] < self.storage[left_child]:
                     self.storage[index], self.storage[left_child] = self.storage[left_child], self.storage[index]
             index += 1
-
-
-h = Heap()
-
-h.insert(6)
-h.insert(7)
-h.insert(5)
-h.insert(8)
-h.insert(10)
-h.insert(1)
-h.insert(2)
-h.insert(5)
-
-
-# print(h.get_size())
-# print(h.delete())
-
-descending_order = []
-
-while h.get_size() > 0:
-    descending_order.append(h.delete())
-
-"""
-l = 2i + 1
-r = 2i + 2
-p = floor((i - 1) / 2)
-"""
